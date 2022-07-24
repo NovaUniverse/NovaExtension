8b0395781cda8281840956ef472caa5cd70ee423
Anton udén <udenanton@hotmail.com>
2022-07-24 19:03:38 +0200

    NMS layer was not needed

--- NovaExtension/src/net/novauniverse/novaextension/NovaExtension.java
+++ NovaExtension/src/net/novauniverse/novaextension/NovaExtension.java
@@ -1,27 +1,19 @@
 package net.novauniverse.novaextension;
 
-import java.io.InvalidClassException;
 import java.util.ArrayList;
 import java.util.List;
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
-import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
 import org.bukkit.event.HandlerList;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
-import net.novauniverse.novaextension.abstraction.NovaExtensionAbstractionLayer;
 import net.novauniverse.novaextension.skin.SkinManager;
 import net.zeeraa.novacore.commons.log.Log;
 import net.zeeraa.novacore.spigot.abstraction.NovaCoreAbstraction;
-import net.zeeraa.novacore.spigot.abstraction.VersionIndependantLoader;
 import net.zeeraa.novacore.spigot.module.ModuleManager;
 
 public class NovaExtension extends JavaPlugin implements Listener {
 	private static NovaExtension instance;
-	private NovaExtensionAbstractionLayer abstractionLayer;
 	private List<NovaExtensionFeature> features;
 
 	public static NovaExtension getInstance() {
@@ -31,10 +23,6 @@ public class NovaExtension extends JavaPlugin implements Listener {
 	@Override
 	public void onLoad() {
 		NovaExtension.instance = this;
-	}
-
-	public NovaExtensionAbstractionLayer getAbstractionLayer() {
-		return abstractionLayer;
 	}
 
 	public boolean hasFeature(NovaExtensionFeature feature) {
@@ -48,22 +36,6 @@ public class NovaExtension extends JavaPlugin implements Listener {
 		String nmsVersion = NovaCoreAbstraction.getNMSVersion();
 		Log.info("NovaExtension", "Server version: " + nmsVersion);
 
-		try {
-			Class<?> clazz = Class.forName("net.novauniverse.novaextension.version." + nmsVersion + ".AbstractionLayer");
-			if (NovaExtensionAbstractionLayer.class.isAssignableFrom(clazz)) {
-				this.abstractionLayer = (NovaExtensionAbstractionLayer) clazz.getConstructor().newInstance();
-				this.abstractionLayer.setOwnerPlugin(this);
-			} else {
-				throw new InvalidClassException(clazz.getName() + " is not assignable from " + VersionIndependantLoader.class.getName());
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-			Log.fatal("NovaExtension", "Could not find support for this CraftBukkit version.");
-
-			Bukkit.getPluginManager().disablePlugin(this);
-			return;
-		}
-
 		if (Bukkit.getServer().getPluginManager().getPlugin("SkinsRestorer") != null) {
 			Log.info("NovaExtension", "Found SkinsRestorer. Enabling SkinManager");
 			ModuleManager.loadModule(this, SkinManager.class, true);
@@ -76,19 +48,5 @@ public class NovaExtension extends JavaPlugin implements Listener {
 	public void onDisable() {
 		Bukkit.getScheduler().cancelTasks(this);
 		HandlerList.unregisterAll((Plugin) this);
-	}
-	
-	@EventHandler
-	public void onPlayerChat(AsyncPlayerChatEvent e) {
-		if (e.getMessage().split(" ")[0].equalsIgnoreCase("applyskin")) {
-			try {
-				Player player = Bukkit.getServer().getPlayer(e.getMessage().split(" ")[1]);
-
-				((SkinManager) ModuleManager.getModule(SkinManager.class)).setSkin(player, e.getMessage().split(" ")[2]);
-			} catch (Exception ex) {
-				e.getPlayer().sendMessage(ChatColor.RED + ex.getClass().getName() + " " + ex.getMessage());
-				ex.printStackTrace();
-			}
-		}
 	}
 }
--- NovaExtenstion-v1_16_R3/src/net/novauniverse/novaextension/version/v1_16_R3/AbstractionLayer.java
+++
@@ -1,7 +0,0 @@
-package net.novauniverse.novaextension.version.v1_16_R3;
-
-import net.novauniverse.novaextension.abstraction.NovaExtensionAbstractionLayer;
-
-public class AbstractionLayer extends NovaExtensionAbstractionLayer {
-
-}
